a37b287b404956397210df8585559f4bfd5afb86
olegdobrynin <dobryi95@gmail.com>
2022-06-07 22:26:16 +0300

    edit art page

--- src/pages/ArtPage.js
+++ src/pages/ArtPage.js
@@ -1,27 +1,39 @@
+import { observer } from 'mobx-react-lite'
 import React, { useEffect, useState } from 'react'
 import { Col, Container, Image, Row } from 'react-bootstrap'
 import { useParams } from 'react-router-dom'
 import { fetchOneArt } from '../http/artAPI'
 
-export default function ArtPage() {
+export default observer( function ArtPage() {
     const [art, setArt] = useState({info: []})
     const {id} = useParams()
-    useEffect(() => {
-        fetchOneArt(id).then(data => setArt(data))
-    })
+     useEffect(() => {
+         fetchOneArt(id).then(data => setArt(data))
+    }, [])
+    const img = process.env.REACT_APP_API_URL + art.img;
+    console.log(art)
   return (
-    <Container>
+    <Container className='mt-3'>
         <Row>
             <Col md={8}>
-                <Image className="w-100" src={process.env.REACT_APP_API_URL + art.img}/>     
+                <Image className="w-100" src={img}/>     
             </Col>
             <Col md={4}>
                 <Row><h2>{art.name}</h2></Row>    
-                <Row>{art.id}</Row> 
+                <Row><h6>{art.about}</h6></Row>
+                <Row><h5>{art.city}</h5></Row> 
+                <Row><h5>{art.year}</h5></Row>
+                <Row>
+                    {art.info.map(info => 
+                            <Row key={info.id}>
+                                <h6>{info.title}: {info.description}</h6>
+                            </Row>
+                        )}    
+                </Row>
             </Col>
         </Row>
         
     </Container>
     
   )
-}
+})
