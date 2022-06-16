4da1f106cde3fc55bbf35bebdb0ed8fd734106ba
olegdobrynin <dobryi95@gmail.com>
2022-06-16 03:11:09 +0300

    Update Main.js

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -13,7 +13,7 @@ const Main = observer( () => {
 
     useEffect( () => {
         fetchTypes().then(data => art.setTypes(data))
-        fetchArts(art.selectedType.id, null, art.page, 3).then(data => {
+        fetchArts(art.selectedType.id, null, art.page, null).then(data => {
             art.setArts(data.rows)
             art.setTotalCount(data.count)
         })
