a814fd2a496e62b9e050c1257dea6f9f57f0d0a7
Konstantin Kuzin <wearebeard@gmail.com>
2022-06-24 02:18:09 +0300

    Hide edit and delete art from other users

--- src/pages/ArtPage.js
+++ src/pages/ArtPage.js
@@ -1,11 +1,12 @@
 import { observer } from 'mobx-react-lite'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useContext, useState } from 'react'
 import { Button, Col, Container, Image, Row } from 'react-bootstrap'
 import { useNavigate, useParams } from 'react-router-dom'
 import { fetchOneArt } from '../http/artAPI'
 import { ARTIST_ROUTE } from '../utils/consts'
 import DeleteArt from '../components/modals/DeleteArt'
 // import EditArt from '../components/modals/EditArt'
+import { Context } from '../index.js';
 
 export default observer( function ArtPage() {
     const navigate = useNavigate()
@@ -14,7 +15,8 @@ export default observer( function ArtPage() {
     const [deleteVisible, setDeleteVisible] = useState(false)
     // const [editVisible, setEditVisible] = useState(false)
     const [img, setImg] = useState()
-    
+    const { user } = useContext(Context);
+
      useEffect(() => {
          fetchOneArt(id).then(data => {
              setArt(data)
@@ -32,45 +34,45 @@ export default observer( function ArtPage() {
                     {art.imgs.map(imgs =>
                         <Col key={imgs} md={3} className="mt-2" onClick={() => {setImg(process.env.REACT_APP_API_URL + "arts/" +imgs)}}><Image className='w-100' src={process.env.REACT_APP_API_URL + "arts/" + imgs}/></Col>
                         )}
-                </Row>   
+                </Row>
             </Col>
             <Col md={4}>
                 <Row>
-                        {art.artists.map(artists => 
+                        {art.artists.map(artists =>
                             <Row key={artists.id} style={{cursor: 'pointer'}} onClick={() => navigate(ARTIST_ROUTE + '/' + artists.id)}>
                                 <h6>{artists.name}</h6>
                             </Row>
-                        )}    
-                </Row>              
+                        )}
+                </Row>
 
-                <Row><h2>{art.name}</h2></Row>    
+                <Row><h2>{art.name}</h2></Row>
                 <Row><h6>{art.about}</h6></Row>
-                <Row><h5>{art.city}</h5></Row> 
+                <Row><h5>{art.city}</h5></Row>
                 <Row><h5>{art.year}</h5></Row>
                 <Row>
-                    {art.properties.map(properties => 
+                    {art.properties.map(properties =>
                             <Row key={properties.id}>
                                 <h6>{properties.title}: {properties.description}</h6>
                             </Row>
-                        )}    
+                        )}
                 </Row>
-                 
-                 <Row>
+
+                 {!user.artists.some(({ id }) => art.artists.some((a) => a.id === id)) || <Row>
                      <Button
                         className='mt-2 mb-2'
-                        variant="outline-danger" 
+                        variant="outline-danger"
                         onClick={() => setDeleteVisible(true)}
                         >
                         Удалить
                     </Button>
                     {/* <Button
                         className='mt-2 mb-2'
-                        variant="outline-dark" 
+                        variant="outline-dark"
                         onClick={() => setEditVisible(true)}
                         >
                         Редактировать
                     </Button> */}
-                </Row>
+                </Row>}
             </Col>
         </Row>
         <DeleteArt show={deleteVisible} onHide={() => setDeleteVisible(false)}/>
