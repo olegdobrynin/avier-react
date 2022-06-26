db7988973bffaaa2c695623933263200b2afe3f8
olegdobrynin <dobryi95@gmail.com>
2022-06-26 18:15:08 +0300

    optimization admin page

--- src/components/ArtistList.js
+++ src/components/ArtistList.js
@@ -1,17 +1,32 @@
 import { observer } from 'mobx-react-lite'
-import React, { useContext } from 'react'
-import { Row } from 'react-bootstrap'
+import React, { useContext, useState } from 'react'
+import { Card, Col, Row } from 'react-bootstrap'
 import { Context } from '../index'
 import ArtistCard from './ArtistCard'
-
+import CreateArtist from './modals/CreateArtist'
 export default observer( function ArtistList() {
-    const {user} = useContext(Context)
+
+  const [artistVisible, setArtistVisible] = useState(false)
+  const {user} = useContext(Context)
+
+
   return (
     <Row className='d-flex pt-2'>
-        {user.artists.map(artist =>
-            <ArtistCard key={artist.id} artist={artist}/>
-            )}
-    </Row>
+      {user.artists.map(artist =>
+        <ArtistCard key={artist.id} artist={artist}/>
+      )}
+      <Col md={3} onClick={() => setArtistVisible(true)}>
+        <Card style={{cursor: 'pointer'}} className='mb-3'>
+          <Card.Img variant="top" src={"/plus.png"} />
+          <Card.Body>
+            <div className='d-flex justify-content-center align-items-center'>
+              <Card.Title>Добавить художника</Card.Title>
+            </div>
+          </Card.Body>
+        </Card>
+      </Col>
+      <CreateArtist show={artistVisible} onHide={() => setArtistVisible(false)}/>
+    </Row>  
   )
 }
 )
--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,22 +1,9 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { Button, Container, Row } from 'react-bootstrap'
-import { Context } from '..'
+import React from 'react'
+import { Container, Row } from 'react-bootstrap'
 import ArtistList from '../components/ArtistList'
-import CreateArt from '../components/modals/CreateArt'
-import CreateArtist from '../components/modals/CreateArtist'
-import { fetchInfo } from '../http/userAPI';
+
 
 export default function Admin() {
-  const [artVisible, setArtVisible] = useState(false)
-  const [artistVisible, setArtistVisible] = useState(false)
-  const {user} = useContext(Context)
-
-  useEffect(() => {
-    fetchInfo(user.userInfo.id).then(data => {
-      user.setArtists(data.artists)
-    })
-  }, [])
-  
 
   return (
     <Container
@@ -25,20 +12,6 @@ export default function Admin() {
       <Row className='px-5'>
         <ArtistList />
       </Row>
-      
-      <Row>
-      <div className="d-flex justify-content-center align-item-center d-grid gap-2">
-  <Button variant="outline-dark" onClick={() => setArtVisible(true)}>
-    Добавить арт
-  </Button>
-  <Button variant="outline-dark" onClick={() => setArtistVisible(true)}>
-    Добавить художника
-  </Button>
-  <CreateArt show={artVisible} onHide={() => setArtVisible(false)}/>
-  <CreateArtist show={artistVisible} onHide={() => setArtistVisible(false)}/>
-</div>
-      </Row>
-      
     </Container>
   )
 }
