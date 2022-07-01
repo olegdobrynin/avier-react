8fdef234cc7af0e88f98e662895698ccfbaa7f22
Konstantin Kuzin <wearebeard@gmail.com>
2022-07-01 16:52:36 +0300

    Lint module

--- src/components/modals/DeleteArtist.js
+++ src/components/modals/DeleteArtist.js
@@ -1,25 +1,24 @@
-import React from 'react'
-import { Button, Modal } from 'react-bootstrap'
-import { useNavigate, useParams } from 'react-router-dom'
-import { deleteArtist } from '../../http/artistAPI'
-import { MAIN_ROUTE } from '../../utils/consts'
+import React from 'react';
+import { Button, Modal } from 'react-bootstrap';
+import { useNavigate, useParams } from 'react-router-dom';
+import { deleteArtist } from '../../http/artistAPI';
+import { MAIN_ROUTE } from '../../utils/consts';
 
-export default function DeleteArtist({show, onHide}) {
-  const navigate = useNavigate()
-  const {id} = useParams()
+export default ({ show, onHide }) => {
+  const navigate = useNavigate();
+  const { id } = useParams();
   const delArtist = () => {
-    deleteArtist(id).then(data => {
-      onHide()
-      alert(data.message)
-      navigate(MAIN_ROUTE)
-     }
-    )
-  }
+    deleteArtist(id).then((data) => {
+      onHide();
+      alert(data.message);
+      navigate(MAIN_ROUTE);
+    });
+  };
 
   return (
     <Modal
-    show={show}
-    onHide={onHide}
+      show={show}
+      onHide={onHide}
       size="lg"
       centered
     >
@@ -33,5 +32,5 @@ export default function DeleteArtist({show, onHide}) {
         <Button variant="danger" onClick={delArtist}>Удалить</Button>
       </Modal.Footer>
     </Modal>
-  )
-}
+  );
+};
