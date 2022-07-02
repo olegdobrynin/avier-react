47e325ba0afa44594eace6a4e4f3fea30e740ef8
Konstantin Kuzin <wearebeard@gmail.com>
2022-07-03 02:54:37 +0300

    Wrap in observer

--- src/pages/Mark.js
+++ src/pages/Mark.js
@@ -1,7 +1,8 @@
+import { observer } from 'mobx-react-lite'
 import React from 'react'
 
-export default function Mark() {
+export default observer(() => {
   return (
     <div>Mark</div>
-  )
-}
+  );
+});
