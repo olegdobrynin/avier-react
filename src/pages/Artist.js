dfc0ec98c08504bc8d9b5d30033af60128ebf279
Konstantin Kuzin <wearebeard@gmail.com>
2022-07-03 14:11:59 +0300

    Navigate to main when artist doesn't exist

--- src/pages/Artist.js
+++ src/pages/Artist.js
@@ -3,26 +3,28 @@ import React, { useContext, useEffect, useState } from 'react';
 import {
   Button, Col, Container, Image, Row,
 } from 'react-bootstrap';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { Context } from '../index.js';
 import ArtList from '../components/ArtList.js';
 import DeleteArtist from '../components/modals/DeleteArtist.js';
 import EditArtist from '../components/modals/EditArtist.js';
 import { fetchArts, fetchOneArtist } from '../http/artAPI.js';
+import { MAIN_ROUTE } from '../utils/consts.js';
 
 export default observer(() => {
+  const navigate = useNavigate();
   const { id } = useParams();
   const { art, user } = useContext(Context);
   const [artist, setArtist] = useState({});
   const [img, setImg] = useState('');
-  const [deleteVisible, setDeleteVisible] = useState(false);
   const [editVisible, setEditVisible] = useState(false);
+  const [deleteVisible, setDeleteVisible] = useState(false);
 
   useEffect(() => {
     fetchOneArtist(id).then((data) => {
       setArtist(data);
       setImg(`${process.env.REACT_APP_API_URL}artists/${data.img}`);
-    });
+    }).catch(() => navigate(MAIN_ROUTE));
     fetchArts(null, id, art.page, null).then((data) => {
       art.setArts(data.rows);
       art.setTotalCount(data.count);
@@ -39,22 +41,22 @@ export default observer(() => {
           <Row><h2>{artist.name}</h2></Row>
           <Row><h6>{artist.bio}</h6></Row>
           {!user.artists.some((artist) => artist.id === Number(id)) || (
-          <Row>
-            <Button
-              className="my-2"
-              variant="outline-dark"
-              onClick={() => setEditVisible(true)}
-            >
-              Редактировать
-            </Button>
-            <Button
-              className="my-2"
-              variant="outline-danger"
-              onClick={() => setDeleteVisible(true)}
-            >
-              Удалить
-            </Button>
-          </Row>
+            <Row>
+              <Button
+                className="my-2"
+                variant="outline-dark"
+                onClick={() => setEditVisible(true)}
+              >
+                Редактировать
+              </Button>
+              <Button
+                className="my-2"
+                variant="outline-danger"
+                onClick={() => setDeleteVisible(true)}
+              >
+                Удалить
+              </Button>
+            </Row>
           )}
         </Col>
       </Row>
