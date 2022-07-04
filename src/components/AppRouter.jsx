5a474f4b3b847bbfde1f5c261264d1c83b2c9341
Konstantin Kuzin <wearebeard@gmail.com>
2022-07-05 00:49:49 +0300

    Add admin routes and filter routes with role

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -2,18 +2,26 @@ import React, { useContext } from 'react';
 import { observer } from 'mobx-react-lite';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { Context } from '../index.jsx';
-import { authRoutes, publicRoutes } from '../routes.jsx';
+import { adminRoutes, authRoutes, publicRoutes } from '../routes.jsx';
 import { LOGIN_ROUTE, MAIN_ROUTE, REGISTRATION_ROUTE } from '../utils/consts.js';
 
 export default observer(() => {
   const { user } = useContext(Context);
 
-  const getRoutes = () => user.isAuth
-    ? [...authRoutes, ...publicRoutes]
-      .filter(({ path }) => path !== REGISTRATION_ROUTE && path !== LOGIN_ROUTE)
-      .map(({ path, element }) => (<Route key={path} path={path} element={element} end />))
-    : publicRoutes
-      .map(({ path, element }) => (<Route key={path} path={path} element={element} end />));
+  const getRoutes = () => {
+    switch (user.info.role) {
+      case 'admin':
+        return [...adminRoutes, ...authRoutes, ...publicRoutes]
+          .map(({ path, element }) => (<Route key={path} path={path} element={element} end />));
+      case 'user':
+        return [...authRoutes, ...publicRoutes]
+          .filter(({ path }) => path !== REGISTRATION_ROUTE && path !== LOGIN_ROUTE)
+          .map(({ path, element }) => (<Route key={path} path={path} element={element} end />));
+      default:
+        return publicRoutes
+          .map(({ path, element }) => (<Route key={path} path={path} element={element} end />));
+    }
+  };
 
   return (
     <Routes>
