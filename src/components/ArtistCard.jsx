f7e120d4497ffaae1120feb425f29c13a14a3129
Konstantin Kuzin <wearebeard@gmail.com>
2022-07-05 01:30:45 +0300

    Add links to items

--- src/components/ArtistCard.jsx
+++ src/components/ArtistCard.jsx
@@ -1,7 +1,7 @@
-import { observer } from 'mobx-react-lite';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Card, Col } from 'react-bootstrap';
+import { observer } from 'mobx-react-lite';
 import { ARTIST_ROUTE } from '../utils/consts.js';
 
 export default observer(({ artist }) => {
@@ -9,14 +9,21 @@ export default observer(({ artist }) => {
   const img = `${process.env.REACT_APP_API_URL}artists/${artist.img}`;
 
   return (
-    <Col md={3} onClick={() => navigate(`${ARTIST_ROUTE}/${artist.id}`)}>
+    <Col md={3}>
       <Card style={{cursor: 'pointer'}} className='mb-3'>
-        <Card.Img variant="top" src={img} />
-        <Card.Body>
-          <div className='d-flex justify-content-center align-items-center'>
-            <Card.Title>{artist.name}</Card.Title>
-          </div>
-        </Card.Body>
+        <Card.Link
+          tabIndex="0"
+          onClick={() => navigate(`${ARTIST_ROUTE}/${artist.id}`)}
+          style={{'text-decoration': "none"}}
+          href="#"
+        >
+          <Card.Img variant="top" src={img} />
+          <Card.Body>
+            <div className='d-flex justify-content-center align-items-center'>
+              <Card.Title style={{color: "black"}}>{artist.name}</Card.Title>
+            </div>
+          </Card.Body>
+        </Card.Link>
       </Card>
     </Col>
   )
--- src/components/ArtistList.jsx
+++ src/components/ArtistList.jsx
@@ -14,14 +14,21 @@ export default observer(() => {
       {user.artists.map((artist) =>
         <ArtistCard key={artist.id} artist={artist} />
       )}
-      <Col md={3} onClick={() => setArtistVisible(true)}>
+      <Col md={3}>
         <Card style={{cursor: 'pointer'}} className='mb-3'>
-          <Card.Img variant="top" src={"/plus.png"} />
-          <Card.Body>
-            <div className='d-flex justify-content-center align-items-center'>
-              <Card.Title>Добавить художника</Card.Title>
-            </div>
-          </Card.Body>
+          <Card.Link
+            tabIndex="0"
+            onClick={() => setArtistVisible(true)}
+            style={{'text-decoration': "none"}}
+            href="#"
+          >
+            <Card.Img variant="top" src={"/plus.png"} />
+            <Card.Body>
+              <div className='d-flex justify-content-center align-items-center'>
+                <Card.Title style={{color: "black"}}>Добавить художника</Card.Title>
+              </div>
+            </Card.Body>
+          </Card.Link>
         </Card>
       </Col>
       <CreateArtist show={artistVisible} onHide={() => setArtistVisible(false)} />
