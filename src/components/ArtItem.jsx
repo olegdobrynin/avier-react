faa650ef4cd0d1d7e27e5b0eb161d0e866d076af
Konstantin Kuzin <wearebeard@gmail.com>
2022-07-11 14:32:02 +0300

    Add links and hide blank rows

--- src/components/ArtItem.jsx
+++ src/components/ArtItem.jsx
@@ -1,30 +1,24 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { Card, Col } from 'react-bootstrap';
 import { observer } from 'mobx-react-lite';
 import MarkCheckbox from './MarkCheckbox.jsx';
 import { ART_ROUTE } from '../utils/consts.js';
 
 export default observer(({ art }) => {
-  const navigate = useNavigate();
   const [checked, setChecked] = useState(art?.mark?.length > 0);
 
   return (
     <Col md={3}>
       <Card style={{ cursor: 'pointer' }} border="light" className="mb-3 w-100">
-        <Card.Link
-          onClick={() => navigate(`${ART_ROUTE}/${art.id}`)}
-          tabIndex="0"
-          style={{ textDecoration: 'none' }}
-          href="#"
-        >
+        <Link to={`${ART_ROUTE}/${art.id}`} tabIndex="0" style={{ textDecoration: 'none' }}>
           <Card.Img className="w-100" src={`${process.env.REACT_APP_API_URL}arts/${art.img}`} />
           <Card.Body>
             <div className="d-flex justify-content-center align-items-center">
               <Card.Title style={{ color: 'black' }}>{art.name}</Card.Title>
             </div>
           </Card.Body>
-        </Card.Link>
+        </Link>
       </Card>
       <MarkCheckbox art={art} checked={checked} setChecked={setChecked} />
     </Col>
--- src/components/ArtistCard.jsx
+++ src/components/ArtistCard.jsx
@@ -1,29 +1,23 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { Card, Col } from 'react-bootstrap';
 import { observer } from 'mobx-react-lite';
 import { ARTIST_ROUTE } from '../utils/consts.js';
 
 export default observer(({ artist }) => {
-  const navigate = useNavigate();
   const img = `${process.env.REACT_APP_API_URL}artists/${artist.img}`;
 
   return (
     <Col md={3}>
       <Card style={{ cursor: 'pointer' }} className="mb-3">
-        <Card.Link
-          tabIndex="0"
-          onClick={() => navigate(`${ARTIST_ROUTE}/${artist.id}`)}
-          style={{ textDecoration: 'none' }}
-          href="#"
-        >
+        <Link tabIndex="0" style={{ textDecoration: 'none' }} to={`${ARTIST_ROUTE}/${artist.id}`}>
           <Card.Img variant="top" src={img} />
           <Card.Body>
             <div className="d-flex justify-content-center align-items-center">
               <Card.Title style={{ color: 'black' }}>{artist.name}</Card.Title>
             </div>
           </Card.Body>
-        </Card.Link>
+        </Link>
       </Card>
     </Col>
   );
