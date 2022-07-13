977b54a62b9044d3796d78826fff0757cb7a87ee
Konstantin Kuzin <wearebeard@gmail.com>
2022-07-13 03:31:30 +0300

    Add dynamic likes count change

--- src/components/LikeCheckbox.jsx
+++ src/components/LikeCheckbox.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { observer } from 'mobx-react-lite';
 import { Form } from 'react-bootstrap';
 import { createLike, deleteLike } from '../http/likeAPI.js';
@@ -8,15 +8,28 @@ export default observer(({
   artId, likes, liked, setLiked,
 }) => {
   const User = useContext(UserContext);
+  const [likesCount, setLikesCount] = useState();
+
+  useEffect(() => {
+    setLikesCount(likes);
+  }, [likes]);
 
   const toggleCheckbox = async () => {
     if (liked) {
-      await deleteLike(User.id, artId);
+      await deleteLike(User.id, artId).then(() => setLikesCount((p) => p - 1));
     } else {
-      await createLike(User.id, artId);
+      await createLike(User.id, artId).then(() => setLikesCount((p) => p + 1));
     }
     setLiked(!liked);
   };
 
-  return <Form.Check className="px-0" checked={liked} label={likes} id="likecb" onChange={toggleCheckbox} />;
+  return (
+    <Form.Check
+      className="px-0"
+      checked={liked}
+      label={likesCount}
+      id="likecb"
+      onChange={toggleCheckbox}
+    />
+  );
 });
